9670a25b598e5f5c5f1f14c127acd70824664aab
NAVAMIRAJ <52597676+NAVAMIRAJ@users.noreply.github.com>
2020-09-08 21:28:12 +0530

    Add files via upload

---
+++ FirstPHPApplication/index.php
@@ -0,0 +1,21 @@
+<title>Navami Raj PHP</title>
+<h1>Navami Raj PHP</h1>
+<?php
+  echo  "The SHA256 hash of \"Navami\" Raj is \n";
+  print hash('sha256', 'Navami Raj');
+?>
+<pre>
+ASCII ART:
+
+    ***     **
+    ****    **
+    ** **   **
+    **  **  **
+    **   ** **
+    **    ****
+    **     ***
+</pre>
+<div>
+  <a href="check.php">Click here to check the error setting </a><br/>
+  <a href="fail.php">Click here to cause a traceback </a>
+</div>
